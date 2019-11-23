055371dbe425cd0f4568f2308521d1e1270db44a
talesboalim <talesboalim@gmail.com>
2019-11-23 17:35:49 -0300

    Incluído repositories Product e Category

---
+++ lib/settings.dart
@@ -0,0 +1,7 @@
+import 'models/user.model.dart';
+
+class Settings {
+  static String apiUrl = "https://balta-eshop.azurewebsistes.net/";
+  static String theme = "light";
+  static UserModel user;
+}
